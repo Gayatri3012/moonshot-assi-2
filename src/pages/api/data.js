0e4b27d24bcc7bd6454cb5bd461f29d4b0533805
Gayatri <takawalegayatri@gmail.com>
2024-10-20 20:09:09 +0530

    used process.cwd() and moved data.csv to public

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,4 +1,4 @@
-import csvParser from "csv-parser";
+import csv from "csv-parser";
 import fs from 'fs';
 import path from "path";
 import { fileURLToPath } from "url";
@@ -9,12 +9,14 @@ export default async function handler(req, res) {
     if(req.method === 'GET'){
     try{
     
-        const __filename = fileURLToPath(import.meta.url);
-        const __dirname = path.dirname(__filename);
-        const filePath = path.resolve(__dirname, "../../util/data.csv");
+        // const __filename = fileURLToPath(import.meta.url);
+        // const __dirname = path.dirname(__filename);
+        // const filePath = path.resolve(__dirname, "../../util/data.csv");
+        const filePath = path.join(process.cwd(), 'public', 'data.csv')
+
             const data = [];
             await fs.createReadStream(filePath)
-            .pipe(csvParser())
+            .pipe(csv())
             .on('data', (row) => data.push(row))
             .on('end', () => {
                 res.status(200).json(data);
